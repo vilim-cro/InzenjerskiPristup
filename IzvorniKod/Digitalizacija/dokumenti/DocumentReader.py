fdc6b9ea40284e56a592886735386a8fb0190c3a
Lovro Mužar <lovromuzar1@gmail.com>
2023-11-02 21:56:55 +0100

    Dodao jednostavni OCR demo

---
+++ IzvorniKod/Digitalizacija/dokumenti/DocumentReader.py
@@ -0,0 +1,19 @@
+import cv2
+from PIL import Image
+import pytesseract
+
+class DocumentReader:
+    __config = r"--psm 3--oem 3"
+    def readDocument(image: Image) -> str:
+        pytesseract.pytesseract.tesseract_cmd = ".OCR_DATA/TESS/tesseract"
+        return pytesseract.image_to_string(image, config=DocumentReader.__config, lang="hrv")
+
+
+'''
+Example of use:
+
+import DocumentReader
+from PIL import Image
+image = Image.open("path/to/image/something.png") (path can be relative or apsolute)
+text = DocumentReader.DocumentReader.readDocument(image)
+'''
