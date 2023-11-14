e8460aad67768dbbffb0282b7c9d9476128e093a
Vilim Branica <vbranica@gmail.com>
2023-11-14 16:04:31 +0100

    Proba s allow all origins

--- IzvorniKod/Digitalizacija/Digitalizacija/settings.py
+++ IzvorniKod/Digitalizacija/Digitalizacija/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://inzenjerskipristup.onrender.com']
+ALLOWED_HOSTS = ['https://inzenjerskipristup.onrender.com', '127.0.0.1']
 
 # Application definition
 
@@ -97,7 +97,8 @@ SIMPLE_JWT = {
 #   "TOKEN_OBTAIN_SERIALIZER": "dokumenti.views.MyTokenObtainPairSerializer",
 # }
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://digitalizacija.surge.sh']
+#CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://digitalizacija.surge.sh']
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', #dodano za CORS
