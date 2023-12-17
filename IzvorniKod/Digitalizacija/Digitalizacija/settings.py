bb0b61f0be7c7df02b260322e64b1154e83ab554
Lovro Mužar <lm54347@fer.hr>
2023-12-17 23:50:23 +0100

    neki cors problem

--- IzvorniKod/Digitalizacija/Digitalizacija/settings.py
+++ IzvorniKod/Digitalizacija/Digitalizacija/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ['inzenjerskipristup.onrender.com']
 ALLOWED_HOSTS = ['*'] if DEBUG else ['inzenjerskipristup.onrender.com', 'localhost', '127.0.0.1']
