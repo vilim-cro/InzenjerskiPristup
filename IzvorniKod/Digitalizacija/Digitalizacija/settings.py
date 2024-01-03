c08b7c4197d23a71df4c675cca1ed13e6f02f831
Lovro Mužar <lm54347@fer.hr>
2024-01-03 23:32:23 +0100

    CORS sigh

--- IzvorniKod/Digitalizacija/Digitalizacija/settings.py
+++ IzvorniKod/Digitalizacija/Digitalizacija/settings.py
@@ -99,8 +99,8 @@ SIMPLE_JWT = {
 #   "TOKEN_OBTAIN_SERIALIZER": "dokumenti.views.MyTokenObtainPairSerializer",
 # }
 
-CORS_ALLOWED_ORIGINS = ['http://127:0.0.1', 'http://localhost:3000'] if DEBUG else ['https://digitalizacija.surge.sh','http://localhost:3000' , 'https://frontendip.onrender.com','http://127:0.0.1', 'http://localhost', 'https://inzinjerskipristup.onrender.com']
-# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ['http://127:0.0.1', 'http://localhost:3000'] if DEBUG else ['https://digitalizacija.surge.sh','http://localhost:3000' , 'https://frontendip.onrender.com','http://127:0.0.1', 'http://localhost', 'https://inzinjerskipristup.onrender.com']
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', #dodano za CORS
     'django.middleware.common.CommonMiddleware',
