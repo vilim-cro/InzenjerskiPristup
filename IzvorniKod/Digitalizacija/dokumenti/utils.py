a6149d4bc3f3621e93e8e6f44107558a44c0927d
Lovro Mužar <lm54347@fer.hr>
2024-01-04 13:28:18 +0100

    onaj isti error...

--- IzvorniKod/Digitalizacija/dokumenti/utils.py
+++ IzvorniKod/Digitalizacija/dokumenti/utils.py
@@ -3,11 +3,10 @@ import requests
 def uploadImage(file):
     upload_url = "https://vgy.me/upload"
     userkey = "pwOQMGygpvrYE2z6m2QeGexlpL9WyvFE"
-
+    test = file.file
     files = {'file': file}
     data = {'userkey': userkey}
     response = requests.post(upload_url, files=files, data=data)
-
     json = response.json()
     if response.status_code == 200:
         return {'url': json['image'], 'delete_url': json['delete']}
