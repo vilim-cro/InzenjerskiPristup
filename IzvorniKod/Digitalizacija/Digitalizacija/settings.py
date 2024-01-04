50e0f354a8ea98584297f0c7186f6680b5a4875e
Lovro Mužar <lm54347@fer.hr>
2024-01-04 15:02:29 +0100

    mozda ce slucajno proradit

--- IzvorniKod/Digitalizacija/Digitalizacija/settings.py
+++ IzvorniKod/Digitalizacija/Digitalizacija/settings.py
@@ -106,7 +106,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
