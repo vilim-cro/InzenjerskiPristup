9dc59431000835b9c85a78e33290a8663dc6ccd5
Lovro Mužar <lm54347@fer.hr>
2024-01-05 13:08:28 +0100

    obecajem da nisam shit

--- IzvorniKod/reactapp/src/constants/constants.js
+++ IzvorniKod/reactapp/src/constants/constants.js
@@ -1,2 +1,2 @@
 // export const url = "https://inzenjerskipristup.onrender.com" /* "http://127.0.0.1:8000" process.env.REACT_APP_BACKEND_URL */ ;
-export const url = "process.env.REACT_APP_BACKEND_URL";
+export const url = process.env.REACT_APP_BACKEND_URL;
