03b60fd89027a9469f6e82cff6f3d93ab0e6cf21
Lovro Mužar <lm54347@fer.hr>
2024-01-08 23:56:59 +0100

    mozda

--- IzvorniKod/Digitalizacija/Digitalizacija/settings.py
+++ IzvorniKod/Digitalizacija/Digitalizacija/settings.py
@@ -34,7 +34,6 @@ DEBUG = env("DEBUG") == "True"
 
 #ALLOWED_HOSTS = ['inzenjerskipristup.onrender.com']
 ALLOWED_HOSTS = [] if DEBUG else ['inzinjerskipristup.onrender.com', 'localhost', '127.0.0.1',"https://inzinjerskipristup.onrender.com"]
-print(ALLOWED_HOSTS)
 # Application definition
 
 INSTALLED_APPS = [
@@ -99,8 +98,8 @@ SIMPLE_JWT = {
 #   "TOKEN_OBTAIN_SERIALIZER": "dokumenti.views.MyTokenObtainPairSerializer",
 # }
 
-# CORS_ALLOWED_ORIGINS = ['http://127:0.0.1', 'http://localhost:3000'] if DEBUG else ['https://digitalizacija.surge.sh','http://localhost:3000' , 'https://frontendip.onrender.com','http://127:0.0.1', 'http://localhost', 'https://inzinjerskipristup.onrender.com']
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['http://127:0.0.1', 'http://localhost:3000'] if DEBUG else ['https://digitalizacija.surge.sh','http://localhost:3000' , 'https://frontendip.onrender.com','http://127:0.0.1', 'http://localhost', 'https://inzinjerskipristup.onrender.com']
+# CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', #dodano za CORS
     'django.middleware.common.CommonMiddleware',
