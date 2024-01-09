4efc5e8a01fdbe249adf6ed56cb113cc3c73b1b5
Marko Selendic <Marko.Selendic@fer.hr>
2024-01-09 16:12:21 +0100

    Cleanup

--- IzvorniKod/reactapp/src/apps/LoginApp.js
+++ IzvorniKod/reactapp/src/apps/LoginApp.js
@@ -5,7 +5,6 @@ import { LoadingButton } from '@mui/lab';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
